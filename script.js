e948522318c4b7ba5dc06aa84b5beda2ad274207
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2024-09-07 15:49:06 -0700

    mix naming corrected

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const processBtn = document.getElementsByClassName("process");
 function crossout(id) {
   let ing = document.getElementById(id);
   ing.classList.add("crossout");
+
+  if (id === "main") {
+    let full = document.getElementById("full");
+    full.classList.remove("hide");
+  }
+
+  if (id === "full") {
+    let full = document.getElementById("bake");
+    full.classList.remove("hide");
+  }
 }
 
 for (let b of processBtn) {
