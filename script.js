6ce0eba725b8be2d3c9c1b9c667fbab2c6b741bc
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2024-09-07 14:43:18 -0700

    recipe page

--- script.js
+++ script.js
@@ -1,82 +1,14 @@
-const btnEl = document.getElementsByClassName("palette");
-const title = document.querySelector("h1");
-const scoreBoard = document.getElementById("score");
-let score = 0;
-scoreBoard.innerHTML = score;
+const processBtn = document.getElementsByClassName("process");
 
-function updateScore(num) {
-  score += num;
-  scoreBoard.innerHTML = score;
+function crossout(id) {
+  let ing = document.getElementById(id);
+  ing.classList.add("crossout");
 }
 
-const selection = ["fire", "rock", "paper", "scissors", "water"];
-
-for (let b of btnEl) {
+for (let b of processBtn) {
   b.addEventListener("click", () => {
-    let comp = selection[randomNum()];
-    let fate = play(b.innerHTML, comp);
-    title.innerHTML = `${fate} - we picked: ${comp}`;
-    title.style.color = `var(--${b.name})`;
+    crossout(b.name);
+    b.innerHTML = "done";
+    b.classList.add("done");
   });
-}
-
-function randomNum() {
-  return Math.floor(Math.random() * 5);
-}
-
-function play(human, robot) {
-  let yourFate = "";
-  if (human === robot) {
-    yourFate = "draw";
-  } else {
-    switch (human) {
-      case "fire":
-        if (robot === "water") {
-          yourFate = "you lose";
-          updateScore(-1);
-        } else {
-          yourFate = "you win";
-          updateScore(1);
-        }
-        break;
-      case "water":
-        if (robot === "fire") {
-          yourFate = "you win";
-          updateScore(1);
-        } else {
-          yourFate = "you lose";
-          updateScore(-1);
-        }
-        break;
-      case "rock":
-        if (robot === "fire" || robot === "paper") {
-          yourFate = "you lose";
-          updateScore(-1);
-        } else {
-          yourFate = "you win";
-          updateScore(1);
-        }
-        break;
-      case "paper":
-        if (robot === "fire" || robot === "scissors") {
-          yourFate = "you lose";
-          updateScore(-1);
-        } else {
-          yourFate = "you win";
-          updateScore(1);
-        }
-        break;
-      case "scissors":
-        if (robot === "fire" || robot === "rock") {
-          yourFate = "you lose";
-          updateScore(-1);
-        } else {
-          yourFate = "you win";
-          updateScore(1);
-        }
-        break;
-    }
-  }
-
-  return yourFate;
 }
