57a2ffce2be5669cdd0250488efab1ac5e32e217
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2024-09-23 22:19:24 -0700

    style guide

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-const descriptions = {
-  forest: "you are here",
-  msaga: "the human",
-  msga: "a digital graffiti wall",
-  sk8: " an open studio",
-  baked: "a bakery in the cloud",
-};
-const btns = document.getElementsByClassName("section");
+const btn = document.getElementById("btn");
 
-function updateBtn(children, name) {
-  for (let child of children) {
-    let innerChild = child.children;
-    for (let inner of innerChild) {
-      inner.innerHTML = inner.innerHTML + "👈🏿";
-    }
-
-    child.classList.add("selected");
-    let desc = document.createElement("p");
-    desc.innerHTML = `${name}`;
-    child.appendChild(desc);
-
-    if (name === descriptions["msaga"]) {
-      let who = document.createElement("p");
-      who.innerHTML = `msaga + bushsk8r = ms@ga`;
-      child.appendChild(who);
-    }
-  }
-}
-
-for (let b of btns) {
-  b.addEventListener("click", () => {
-    updateBtn(b.children, descriptions[b.name]);
-    b.setAttribute("disabled", "disabled");
-  });
-}
+btn.addEventListener("click", () => {
+  btn.innerHTML = "this is a clicked button";
+  btn.classList.add("clicked");
+});
