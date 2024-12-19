35d3e9dc559250eb5f2e7bf885874034e5acf60b
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2024-12-19 15:53:01 -0800

    highlight reel v0

--- archive.js
+++ archive.js
@@ -10,62 +10,84 @@ class archiveItem {
     this.craft = item[2];
 
     //item location
-    this.source = item[3];
+    this.source = `https://arweave.net/${item[3]}`;
+
+    //source file type
+    this.fType = item[4];
   }
 }
 
 // key = year
-// value = [curation, craft, source]
+// value = [curation, craft, source, file type]
 
 const crate = {
   //[start year, end year]
   range: [2013, 2024],
-  2024: [
-    ["cul de sac vibes", "pirate radio", ""],
-    ["war ready", "playlist", ""],
-    ["deerverse", "metaverse"]
-  ],
-  2023: [["echo chamber 2.0", "instrumentation / audio recording"]],
-  2022: [["home bakers music club", "radio"]],
-  2021: [
-    ["echo chamber 1.0", "instrumentation / audio recording"],
-    ["playin with poly", "low poly graphics"],
-  ],
-  2020: [
-    ["study", "playlist"],
-    ["human in the loop (vol 1)", "audio recording"],
-  ],
-  2019: [
-    ["vibes", "playlist"],
-    ["how to sk8 and not die", "album"],
-  ],
   2018: [
-    ["marisol", "character design"],
-    ["life = true", "animation"],
-    ["crawl", "playlist"],
-    ["up on a tuesday", "playlist"],
-    ["baked chronicles", "playlist / zine"],
-    ["african orchestra", "playlist"],
-    ["throwback thursday", "playlist"],
-    ["frank mondays", "playlist"],
-    ["the forest", "metaverse / radio"],
+    [
+      "marisol",
+      "character design",
+      "6PqtBb2Aa-CpWLEGvt6ODIq4zG8PMmdNDjpL-mk6sd0",
+      "video",
+    ],
+    [
+      "life = true",
+      "animation",
+      "RTRXhvNqhvm4jRf_Q6V1wtorg9nYs9c4zW_euxL_kRk",
+      "video",
+    ],
+    [
+      "head in the cloud",
+      "animation",
+      "8_jxOoiyBImgCLmXNBDtsdP_mwO1A0_PNqm8tjIedvM",
+      "video",
+    ],
+    [
+      "collected",
+      "animation",
+      "EV4Ks4PYNbT_03vfYjsJGnClKqDFxSlXcS7nrsfObPI",
+      "video",
+    ],
+    [
+      "baked chronicles",
+      "playlist / zine",
+      "S2kjRTpqYE27CwsjhBFE3HbqjVVP0kJ5VOuEuIRz8pU",
+      "img",
+    ],
   ],
   2017: [
-    ["ms_ga", "playlist / character design"],
-    ["let there be light", "mixtape"],
+    [
+      "let there be light",
+      "mixtape",
+      "BOGArivNqK0SjBl-HztcoFfFEXIX3joXM5nNfMLz5Jo",
+      "video",
+    ],
   ],
   2016: [
-    ["soundtrack to the week", "playlist"],
-    ["poly punk", "low poly graphics"],
-    ["welcome", "algorithmic composition"],
+    [
+      "poly punk",
+      "low poly graphics",
+      "goE9lQYG-HwQPTi0nF8xZCku_TILhg_nwOvU1iROGvQ",
+      "img",
+    ],
+    [
+      "welcome",
+      "algorithmic composition",
+      "goE9lQYG-HwQPTi0nF8xZCku_TILhg_nwOvU1iROGvQ",
+      "video",
+    ],
   ],
-  2013: [["bushsk8r", "character design"]],
+  2013: [
+    [
+      "bushsk8r",
+      "character design",
+      "4GJuqrg_zxEnSISGc-djC40eN3li_OoZFUtv2xlLr9Y",
+      "img",
+    ],
+  ],
   getFromYear(year) {
     //return items from year
     return this[year];
-  },
-  getFull: function () {
-    //return full crate
   },
 };
 
--- script.js
+++ script.js
@@ -3,30 +3,6 @@ import curator from "./archive.js";
 //display element from doc
 const displayArea = document.getElementById("dataList");
 
-function displayCuration(year) {
-  let items = curator(year);
-  displayArea.innerHTML = "";
-
-  //add archive items to display area data list
-  for (let arcItem of items) {
-    const arcCuration = createElem("dt", arcItem.curation);
-    const div = createElem("div", "", true, arcItem.curation);
-    const arcCraft = createElem("dd", arcItem.craft);
-    const yearOfCreation = createElem("dd", arcItem.creation);
-    addToTag(div, [arcCraft, yearOfCreation]);
-    addToTag(displayArea, [arcCuration, div]);
-  }
-
-  //add event handlers to archive item titles
-  const titles = document.getElementsByTagName("dt");
-  for (let t of titles) {
-    //display selected descriptors
-    t.addEventListener("click", () => {
-      clickedTitle(t);
-    });
-  }
-}
-
 //create html element
 //inputs(element type, text context, hidden if true, element name)
 function createElem(type, text, hide, name) {
@@ -42,10 +18,61 @@ function createElem(type, text, hide, name) {
   return elem;
 }
 
+//create media element
+function createMedia(type, source) {
+  let elem = "";
+
+  switch (type) {
+    case "video":
+      elem = document.createElement(type);
+      elem.setAttribute("src", source);
+      elem.setAttribute("controls", "controls");
+      elem.classList.add("media");
+      return elem;
+
+    case "img":
+      elem = document.createElement(type);
+      elem.setAttribute("src", source);
+      elem.classList.add("media");
+      return elem;
+
+    default:
+      elem = document.createElement("p");
+      elem.innerHTML = "private collection";
+      //elem.classList.add("media");
+      return elem;
+  }
+}
+
 //add children to html element
 function addToTag(tag, comps) {
   for (let c of comps) {
     tag.appendChild(c);
+  }
+}
+
+function displayCuration(year) {
+  let items = curator(year);
+  displayArea.innerHTML = "";
+
+  //add archive items to display area data list
+  for (let arcItem of items) {
+    const arcCuration = createElem("dt", arcItem.curation);
+    const div = createElem("div", "", true, arcItem.curation);
+    const arcCraft = createElem("dd", arcItem.craft);
+    const yearOfCreation = createElem("dd", arcItem.creation);
+    const item = createMedia(arcItem.fType, arcItem.source);
+    addToTag(div, [arcCraft, yearOfCreation, item]);
+    addToTag(displayArea, [arcCuration, div]);
+  }
+
+  //add event handlers to archive item titles
+  const titles = document.getElementsByTagName("dt");
+  for (let t of titles) {
+    //display selected descriptors
+    t.addEventListener("click", () => {
+      clickedTitle(t);
+    });
   }
 }
 
